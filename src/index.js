75eedc67602c96ace661d82f3e26f6018cdc80cc
ZipJo <login@jonas-brueggen.de>
2020-11-23 22:44:41 +0100

    added .eslintrc rules

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ async function print() {
   // Here we are using dynamic import
   const { greet } = await import('./greet');
   const response = await greet('John Doe');
+
   console.log(response);
 }
 
