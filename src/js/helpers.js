31e9076c6708121c497f14e60fd2d8990dee248c
ZipJo <login@jonas-brueggen.de>
2020-11-29 18:54:25 +0100

    added helper file and link-replacement function

---
+++ src/js/helpers.js
@@ -0,0 +1,42 @@
+const LINKS = {
+    website: 'https://klima-mitbestimmung.jetzt/',
+    petition: 'https://epetitionen.bundestag.de/content/petitionen/_2020/_09/_16/Petition_116046.html',
+    constact: 'https://klima-mitbestimmung.jetzt/kontakt',
+    press: 'https://klima-mitbestimmung.jetzt/presse',
+    faq: 'https://klima-mitbestimmung.jetzt/faq',
+    instagram: 'https://www.instagram.com/klimamitbestimmung/',
+    facebook: 'https://www.facebook.com/KlimaMitbestimmungJETZT/',
+    twitter: 'https://twitter.com/KMitbestimmung',
+    imprint: 'https://klima-mitbestimmung.jetzt/impressum',
+};
+
+export const addAutoplayVideoModal = () => {
+    document.querySelectorAll('[data-toggle="modal"][data-youtube]').forEach((modalTrigger) => {
+        const modalElem = document.querySelector(modalTrigger.getAttribute('data-target'));
+        const src = modalTrigger.getAttribute('data-youtube');
+        const srcAuto = `${src}?autoplay=1`;
+        if (!modalElem || !src) return;
+
+        // for whatever weird reason, bootstrap events don't seem to fire - using a mutationObserver instead
+        const modalObserver = new MutationObserver((mutations) => {
+            mutations.forEach((mu) => {
+                if (!mu.target.classList.contains('show')) {
+                    modalElem.querySelector('iframe').setAttribute('src', src);
+                }
+            });
+        });
+
+        modalTrigger.addEventListener('click', () => {
+            modalElem.querySelector('iframe').setAttribute('src', srcAuto);
+        });
+        modalObserver.observe(modalElem, { attributes: true, attributeFilter: ['class'] });
+    });
+};
+
+export const setLinks = () => {
+    document.querySelectorAll('[data-href]').forEach((a) => {
+        const link = LINKS[a.getAttribute('data-href')];
+        if (!link) return console.warn(a.getAttribute('data-href'), 'is not a valid link:', link);
+        return a.setAttribute('href', link);
+    });
+};
